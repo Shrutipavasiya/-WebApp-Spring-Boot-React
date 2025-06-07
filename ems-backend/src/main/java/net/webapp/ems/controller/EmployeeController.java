f7848eece24175be03e2b4465ef02c403e321c8d
Shrutipavasiya <shrutipavasiya17012002@gmail.com>
2025-06-07 18:07:39 -0400

    Get All employees REST API

--- ems-backend/src/main/java/net/webapp/ems/controller/EmployeeController.java
+++ ems-backend/src/main/java/net/webapp/ems/controller/EmployeeController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/employees")
@@ -35,6 +37,12 @@ public class EmployeeController {
     public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeID){
         EmployeeDto employeeDto = employeeService.getEmployeeByID(employeeID);
         return ResponseEntity.ok(employeeDto);
+    }
 
+    //build Get All employee REST API
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
+        List<EmployeeDto> employees = employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
     }
 }
--- ems-backend/src/main/java/net/webapp/ems/service/EmployeeService.java
+++ ems-backend/src/main/java/net/webapp/ems/service/EmployeeService.java
@@ -2,8 +2,11 @@ package net.webapp.ems.service;
 
 import net.webapp.ems.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
     EmployeeDto getEmployeeByID(Long employeeId);
+    List<EmployeeDto> getAllEmployees();
 }
 
